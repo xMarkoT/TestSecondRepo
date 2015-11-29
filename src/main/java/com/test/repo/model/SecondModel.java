e6dd3f6a41c9e33f502da4bed7ffc08c1234cd6f
xMarkoT <marko.trajcheski@gmail.com>
2015-11-29 20:11:08 +0100

    My Second Commit

---
+++ src/main/java/com/test/repo/model/SecondModel.java
@@ -0,0 +1,15 @@
+package com.test.repo.model;
+
+public class SecondModel {
+
+	private String myStr;
+
+	public String getMyStr() {
+		return myStr;
+	}
+
+	public void setMyStr(String myStr) {
+		this.myStr = myStr;
+	}
+	
+}
